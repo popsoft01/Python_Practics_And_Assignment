8823850037bcdc4e67623d2eb246ab4e873c8b53
popsoft01 <popoolatunde52@gmail.com>
2021-07-24 12:45:31 +0100

    new python project

--- AccountOOP.py
+++ AccountOOP.py
@@ -18,8 +18,18 @@ class Account:
             raise TypeError("invalid amount")
 
     def withdraw(self, amount):
+        if amount > Decimal(0.0):
+            self.balance -= amount
+        else:
+            raise TypeError("invalid amount")
+
+    def check_balance(self):
+        return self.balance
 
 
 tunde = Account("Tunde", "Pop", 1000)
 tunde.deposit(2000)
+tunde.deposit(4000)
+tunde.withdraw(1000)
+
 print(str(tunde))
