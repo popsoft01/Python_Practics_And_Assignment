2f9cd5ff717d3f56c6e522781a107980ad3e0e78
popsoft01 <popoolatunde52@gmail.com>
2021-08-12 23:33:19 +0100

    new python project

--- check_substring_num.py
+++ check_substring_num.py
@@ -1,8 +1,15 @@
 def string_check(word):
-    l_num =0
+    l_num = 0
     for letter in range(len(word)):
         find_letter = " "
         for j in range(letter, len(word)):
             if word[letter] not in find_letter:
                 find_letter = word[j]
-                l_num = max(l_num,len(word))
+                l_num = max(l_num, len(find_letter))
+            else:
+                break
+    return l_num
+
+
+sprint = string_check("bessyjane")
+print(sprint)
